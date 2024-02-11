d3ef7f826117062dce7ad3e7af13cb71a9500fc3
elamribadrayour <badrayour.elamri@protonmail.com>
2024-02-11 20:55:32 +0100

    [UPDATE]

--- llm_index/load/gcp/__init__.py
+++ llm_index/load/gcp/__init__.py
@@ -1,1 +1,4 @@
 """helpers to load data from google cloud platform."""
+
+import storage  # noqa F401
+import bigquery  # noqa F401
--- llm_index/load/gcp/bigquery.py
+++ llm_index/load/gcp/bigquery.py
@@ -1,15 +1,16 @@
 """BigQuery data loader."""
 
+import logging
+
 import pandas
 from google.cloud import bigquery
 
 
-def get_data(query: str, gcloud_project: str) -> pandas.DataFrame:
+def get_data(query: str | None, project: str) -> pandas.DataFrame:
     """Get data from bigquery."""
-    data_frame = (
-        bigquery.Client(project=gcloud_project)
-        .query(query=query)
-        .result()
-        .to_dataframe()
-    )
-    return data_frame
+    if query is None:
+        raise ValueError("You need to set a sql query to run in BigQuery.")
+
+    logging.info(f"load data from google cloud bigquery project={project}")
+    client = bigquery.Client(project=project)
+    return client.query(query=query).result().to_dataframe()
