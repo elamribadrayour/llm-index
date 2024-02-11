3a673114b658cda1e64ae1ef09e63ff303064f6e
baelamri-ankorstore <badrayour.elamri@protonmail.com>
2024-02-11 11:36:47 +0100

    [FEATURE]

---
+++ src/llm-index/load/base.py
@@ -0,0 +1,10 @@
+"""Base functions to load data."""
+
+import pandas
+
+
+def load(
+    inputs_type: str,
+) -> pandas.DataFrame:
+    raise NotImplementedError
+    return pandas.DataFrame()
---
+++ src/llm-index/load/gcp/__init__.py
@@ -0,0 +1,1 @@
+"""helpers to load data from google cloud platform."""
